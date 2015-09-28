7fa45c1b01428d5f8b8ce37e2100d8013287f739
Henning Kopp <henning.kopp@uni-ulm.de>
2015-09-28 13:58:27 +0200

    main is more verbose

--- main.cpp
+++ main.cpp
@@ -7,14 +7,17 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
+    cout << "The following numbers should increase and the one in the middle should be 0" << endl;
     cout << Random::MoroInverseCumulativeNormal(0.1) << endl;
     cout << Random::MoroInverseCumulativeNormal(0.5) << endl;
     cout << Random::MoroInverseCumulativeNormal(0.8) << endl;
+    cout << "The following numbers should increase" << endl;
     cout << Random::CumulativeNormal(0) << endl;
     cout << Random::CumulativeNormal(1) << endl;
     cout << Random::CumulativeNormal(2) << endl;
     cout << Random::CumulativeNormal(3) << endl;
     cout << Random::CumulativeNormal(10) << endl;
+    cout << "Price of a call option" << endl;
     Forward f (1.0);
     cout << f.GetPrice(2.0,3.0,4.0,5.0,1.0) << endl;
     return 0;
