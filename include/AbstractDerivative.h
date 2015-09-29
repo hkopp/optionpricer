5ad2e348dec7c9cee2871620f64c16eee7523927
Henning Kopp <henning.kopp@uni-ulm.de>
2015-09-29 14:53:45 +0200

    doc for AbstractDerivative and Random

--- include/AbstractDerivative.h
+++ include/AbstractDerivative.h
@@ -9,8 +9,25 @@ class AbstractDerivative
 {
     public:
         AbstractDerivative();
+
+        //! returns the analytical Black-Scholes price of the asset
+        /*!
+         * The inputs are the parameters needed in the Black-Scholes model, namely
+         * \param expiryT The expiry time
+         * \param rater The continuous computing rate
+         * \param dividend The dividend rate
+         * \param spot The spot price of the underlying
+         * \param sigma The Volatility sigma
+         * \return the analytical price of the derivative
+         */
         virtual double GetPrice(double expiryT, double rater, double dividend, double spot, double sigma) const = 0; //pure virtual function which has to be implemented
-        virtual double Payoff(double currentspot) = 0; //!< The Payoff given the current spot.
+
+        //! The Payoff of the derivative
+        /*!
+         * \param currentspot The current spot price of the underlying
+         * \return the payoff of the derivative
+         */
+        virtual double Payoff(double currentspot) = 0;
     protected:
     private:
 };
--- include/Random.h
+++ include/Random.h
@@ -1,11 +1,17 @@
 #ifndef RANDOM_H
 #define RANDOM_H
 
-
+//! A class for stochastic functions
+/*!
+ * This class provides often-needed stochastic functions
+ */
 class Random
 {
     public:
+		//! The inverse cumulative normal function due to Moro
         static double MoroInverseCumulativeNormal(double);
+
+        //! The cumulative normal function
         static double CumulativeNormal(double);
     protected:
     private:
