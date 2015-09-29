230239b16824faa97d0ef4a43dfe3ea24ca2ae2d
Henning Kopp <henning.kopp@uni-ulm.de>
2015-09-29 16:41:43 +0200

    EuropeanCallOption should now be significantly faster.
    But now the result changed. Floating point error?

--- src/EuropeanCallOption.cpp
+++ src/EuropeanCallOption.cpp
@@ -17,7 +17,8 @@ EuropeanCallOption::EuropeanCallOption(double strikeK)
 double EuropeanCallOption::GetPrice(double expiryT, double rater, double dividend, double spot, double sigma) const
 {
 	double d1=(std::log(spot/strikeK)+(rater-dividend+1/2*std::pow(sigma,2))*expiryT)/(sigma*std::sqrt(expiryT));
-	double d2=(std::log(spot/strikeK)+(rater-dividend-1/2*std::pow(sigma,2))*expiryT)/(sigma*std::sqrt(expiryT));
+	double d2=d1-sigma*std::sqrt(expiryT);
+//	double d2=(std::log(spot/strikeK)+(rater-dividend-1/2*std::pow(sigma,2))*expiryT)/(sigma*std::sqrt(expiryT));
 	return spot*std::exp(-dividend*expiryT)*Random::CumulativeNormal(d1) - strikeK*std::exp(-rater*expiryT)*Random::CumulativeNormal(d2);
     
 }
