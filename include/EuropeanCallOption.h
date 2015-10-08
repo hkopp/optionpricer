938c22ee2e30ab18e7e9190148bcbd105a1bec23
Henning Kopp <henning.kopp@uni-ulm.de>
2015-10-08 07:51:48 +0200

    documented European Call

--- include/EuropeanCallOption.h
+++ include/EuropeanCallOption.h
@@ -9,8 +9,17 @@ class EuropeanCallOption : public AbstractDerivative
     public:
         //EuropeanCallOption();
 
-        //! constructs a European Call option with a strike price
+        //! constructs a European Call option with a strike price and expiry time
         EuropeanCallOption(double strikeK, double expiryT);
+        //! returns the analytical price of the call option
+        /*!
+         * The inputs are the parameters needed in the Black-Scholes model, namely
+         * \param rater The continuous computing rate
+         * \param dividend The dividend rate
+         * \param spot The spot price of the underlying
+         * \param sigma The Volatility sigma
+         * \return the analytical price of the call
+         */
         double GetPrice(double rater, double dividend, double spot, double sigma) const;
         double Payoff(double currentspot) const;
         double GetExpiry() const;
