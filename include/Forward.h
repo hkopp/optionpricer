3203e83575f20f79a931fb6c5fc0f289a9aa194f
Henning Kopp <henning.kopp@uni-ulm.de>
2015-10-09 07:45:39 +0200

    Forward documented

--- include/Forward.h
+++ include/Forward.h
@@ -3,12 +3,23 @@
 
 #include "AbstractDerivative.h"
 
-
+//! This class implements Forwards
 class Forward : public AbstractDerivative
 {
     public:
         Forward();
+
+        //! constructs a Forward with a strike price and expiry time
         Forward(double strikeK, double expiryT);
+        //! returns the analytical price of a forward
+        /*!
+         * The inputs are the parameters needed in the Black-Scholes model, namely
+         * \param rater The continuous computing rate
+         * \param dividend The dividend rate
+         * \param spot The spot price of the underlying
+         * \param sigma The Volatility sigma
+         * \return the analytical price of the forward
+         */
         double GetPrice(double rater, double dividend, double spot, double sigma) const;
         double Payoff(double currentspot) const;
         double GetExpiry() const;
