b602efd899413e3a6bdf9622e776dd22c1ea2c60
Henning Kopp <henning.kopp@uni-ulm.de>
2015-10-30 07:50:19 +0100

    new test for EuropeanCallOptions

--- test/test_EuropeanCallOption.cpp
+++ test/test_EuropeanCallOption.cpp
@@ -18,3 +18,15 @@ BOOST_AUTO_TEST_CASE(Trivial_Price)
 	BOOST_CHECK_CLOSE(c1.Payoff(10.0), 0.0, 0.00001);
 	BOOST_CHECK_CLOSE(c1.Payoff(70.0), 20.0, 0.00001);
 }
+
+BOOST_AUTO_TEST_CASE(Monotony_Strike)
+//The price of a call option should be monotonously decreasing with strike
+{
+	EuropeanCallOption c1 (30.0, 10.0);
+	EuropeanCallOption c2 (40.0, 10.0);
+	EuropeanCallOption c3 (50.0, 10.0);
+	EuropeanCallOption c4 (60.0, 10.0);
+	BOOST_CHECK(c1.Payoff(50.0) >= c2.Payoff(50.0));
+	BOOST_CHECK(c2.Payoff(50.0) >= c3.Payoff(50.0));
+	BOOST_CHECK(c3.Payoff(50.0) >= c4.Payoff(50.0));
+}
