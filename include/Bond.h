17c8b88973555c8022810483704e04eccc5b7a02
Henning Kopp <henning.kopp@uni-ulm.de>
2015-11-15 20:23:18 +0100

    documentation for bonds

--- include/Bond.h
+++ include/Bond.h
@@ -1,12 +1,20 @@
 #ifndef BOND_H
 #define BOND_H
 
+//! A class for Bonds
+/*!
+ * This class provides an implementation of Bonds
+ */
 class Bond
 {
     public:
+		//! Constructor. Creates a Bond who has value facevalue at time expiryT.
 		Bond(double facevalue, double expiryT);
+		//! Computes the price of the bond.
         double GetPrice(double rater) const;
+        //! Returns the face value of the Bond, i.e., the payoff at expiry.
         double Payoff() const;
+        //! Returns the expiry time.
         double GetExpiry() const;
     protected:
     private:
